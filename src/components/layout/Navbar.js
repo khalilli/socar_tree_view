3e09f05a7e07ed91c4def6f019072b5cd8180028
luthien017 <esmerkhalilli@gmail.com>
2022-06-20 15:11:30 +0400

    k

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,16 +5,16 @@ const Navbar = () => {
         <nav className="navbar">
             <ul className="navbar-list">
                 <li className="navbar-list--item">
-                    <Link to={window.baseUrl + 'app/cd' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Company Details</Link>
+                    <Link to={window.baseUrl + 'editapp/cd' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Company Details</Link>
                 </li>
                 <li className="navbar-list--item">
-                    <Link to={window.baseUrl + 'app/bm' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Board Members</Link>
+                    <Link to={window.baseUrl + 'editapp/bm' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Board Members</Link>
                 </li>
                 <li className="navbar-list--item">
-                    <Link to={window.baseUrl + 'app/fd' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Financial Data</Link>
+                    <Link to={window.baseUrl + 'editapp/fd' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Financial Data</Link>
                 </li>
                 <li className="navbar-list--item">
-                    <Link to={window.baseUrl + 'app/sc' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Share of Companies</Link>
+                    <Link to={window.baseUrl + 'editapp/sc' + '?func=ll&objId=' + window.currentWebreportId + '&objAction=RunReport'} className="navbar-list--link">Share of Companies</Link>
                 </li>
             </ul>
         </nav>
