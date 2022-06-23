a2e470335dee4d399654c25e91d726755a7b689a
luthien017 <esmerkhalilli@gmail.com>
2022-06-23 10:30:35 +0400

    iii

--- src/feautures/api/initiateWorkflow.js
+++ src/feautures/api/initiateWorkflow.js
@@ -11,17 +11,14 @@ const initiateWorkflow = async (args) => {
     args.share && data.append("share", args.share);
     args.numberOfEmployees && data.append("numberOfEmployees", args.numberOfEmployees);
     args.ceo && data.append("ceo", args.ceo);
-
     args.directorLength && data.append("directorLength", args.directorLength);
     for(var j=0; j<args.directorLength; j++){
       args.directorList[j] && data.append("directorList"+[j+1], args.directorList[j]);
     }
-
     args.representativeLength && data.append("representativeLength", args.representativeLength);
     for(var k=0; k<args.representativeLength; k++){
       args.representativeList[k] && data.append("representativeList"+[k+1], args.representativeList[k]);
     }
-    console.log(args.color, args.colorC, args.colorL);
     if (args.color === true){
       args.active && data.append("active", args.active);
     }
@@ -31,13 +28,24 @@ const initiateWorkflow = async (args) => {
     if(args.colorL === true){
       args.liability && data.append("liability", args.liability);
     }
-    args.ebitda && data.append("ebitda", args.ebitda);
-    args.loss && data.append("loss", args.loss);
-    args.incometax && data.append("incometax", args.incometax);
-    args.propertytax && data.append("propertytax", args.propertytax);
-    args.dividend1 && data.append("dividend1", args.dividend1);
-    args.dividend2 && data.append("dividend2", args.dividend2);
-
+    if(args.colorE === true){
+      args.ebitda && data.append("ebitda", args.ebitda);
+    }
+    if(args.colorLoss === true){
+      args.loss && data.append("loss", args.loss);
+    }
+    if(args.coloriT === true){
+      args.incometax && data.append("incometax", args.incometax);
+    }
+    if(args.colorpT === true){
+      args.propertytax && data.append("propertytax", args.propertytax);
+    }
+    if(args.colorD1 === true){
+      args.dividend1 && data.append("dividend1", args.dividend1);
+    }
+    if(args.colorD2 === true){
+      args.dividend2 && data.append("dividend2", args.dividend2);
+    }
     args.nameLength && data.append("nameLength", args.nameLength);
     for(var i=0; i<args.nameLength; i++){
       args.companyName[i] && data.append("companyName"+[i+1], args.companyName[i]);
