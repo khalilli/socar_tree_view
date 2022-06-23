a3539bdeef4b690e053fe156303cfd36ba45b0ba
luthien017 <esmerkhalilli@gmail.com>
2022-06-23 12:24:44 +0400

    mmm

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -205,16 +205,10 @@ const handleGetCompanyShares = async (set, get) => {
     if(!Number.isNaN(key)){
         const response = await axios.get("http://192.168.14.33/otcs/llisapi.dll?func=ll&objId=128820&objAction=RunReport&key="+key);
         let data = response.data;
-        const dataWarn = response.data;
-        console.log(response.data);
-        console.log(data);
-        console.log(dataWarn);
         data.pop();
-        console.log(data);
-        console.log(dataWarn);
         set({
             companyShares: data,
-            companySharesWarn: dataWarn,
+            companySharesWarn: response.data,
         })
     }
 }
